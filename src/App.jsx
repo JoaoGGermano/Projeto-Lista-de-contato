3b00149b92da2882820c25a4d48c08116d38e342
João Germano <testando123testando13@gmail.com>
2025-04-25 22:00:36 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import ContactItem from './components/ContactItem'
+
+function App() {
+const contatcs =[
+  {id:1,name:"João",phone:"(69) 95465-5482",mail:"email@mail.com"},
+  {id:2,name:"Guilherme",phone:"(69) 98755-4565",mail:"atumalaka@mail.com"},
+];
+
+  return (
+    <>
+     <h1>Agenda de contatos</h1>
+     <ContactItem></ContactItem>
+
+  
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ContactItem.jsx
@@ -0,0 +1,12 @@
+function ContactItem(contacts){
+    return <>  
+    <h2>Mario</h2>
+    <p>(69) 90054-7854</p>
+    <p>mail@mail.com</p>
+    <div> 
+    <button>Editar</button>
+    <button>Deletar</button>
+    </div>
+    </>
+}
+export default ContactItem;
